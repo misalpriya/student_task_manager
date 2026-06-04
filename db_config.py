8a275a3a170ffa40fe6018db99e3944cca2c27c8
misalpriya <misalpriya9@gmail.com>
2026-06-04 23:16:41 +0530

    Updated database configuration

--- db_config.py
+++ db_config.py
@@ -2,9 +2,18 @@ import mysql.connector
 
 def get_database_connection():
     connection=mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='priya@1206',
-        database='student_tasks_manager'
+        host='gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
+        user='36dmhzzv3UFMCEB.root',
+        password='SOVJ1HS8AmsAG9H5',
+        database='student_task_manager'
     )
     return connection
+
+# # def get_database_connection():
+#     connection=mysql.connector.connect(
+#         host='localhost',
+#         user='root',
+#         password='priya@1206',
+#         database='student_tasks_manager'
+#     )
+#     return connection
